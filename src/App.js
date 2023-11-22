9402b7ce8a043738c34094c62a2b5462cedb684a
ReeC <cadditz@gmail.com>
2023-11-22 09:39:31 +0700

    version-1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload DEPLOY SUCCESSFUL.
+          Edit <code>src/App.js</code> App version-1.
         </p>
         <a
           className="App-link"
