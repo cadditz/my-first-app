9e6560ec6c577e38ddf8e8046939e3cc710f882f
ReeC <cadditz@gmail.com>
2023-11-22 10:31:23 +0700

    nothing app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="col-9 col-sm-12 container">
+        <h1>Ree.C</h1>
+        <section class="trade">
+        <h3>Limit Order</h3>
         <p>
-          Edit <code>src/App.js</code> App version-1.
+            We all do the same form of analysis. We all see the same charts. There are none of us who possesses special insider information, which will give us an edge. It means we are all in the same boat.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+            In my way thinking, I am reasoning, that if everyone reads the same books, do the same form of analysis, work on the same form of trade execution and profit taking, then is there really space for all of us to profit?
+            I trade in my system, and I always respect my system
+        </p>
+        <blockquote>
+            <p>
+                <span class="quote">&#8220</span>Most importantly, I didn’t hesitate the next time <span class="signal">my signal</span> came, even though I was minus 200 for the last 4 trades.               
+                 </p>
+            <author>
+                Tom Hougher
+            </author>
+        </blockquote>
+        </section>
+        <section class='psychology'>
+            <h3>Sheep</h3>
+            <blockquote>
+                <p>
+                <span class="quote">&#8220</span>I have spent my whole life scared. Frightened of things that could happen; might happen; might not happen. 50 years I've spent like that.
+                </p>
+                <author>
+                    Walter White
+                </author>
+            </blockquote>
+            
+        </section>
     </div>
+    
   );
 }
 
