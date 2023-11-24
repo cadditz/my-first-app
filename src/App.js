4b972bff6baba2164e4b1ed32c0417e0263f12a4
cadditz <109255126+cadditz@users.noreply.github.com>
2023-11-24 18:55:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload DEPLOY SUCCESSFUL.
+          Edit <code>src/App.js</code> and save to reload DEPLOY SUCCESSFUL Character.
         </p>
         <a
           className="App-link"
